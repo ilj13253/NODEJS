930790d56eee2f669d08859d676e027e6b367ce8
nero ir <butackovilj25@gmail.com>
2024-10-24 17:19:36 +0800

    TOP

---
+++ Новая папка/Es/index.mjs
@@ -0,0 +1,4 @@
+const cowsay = require("smy_package.mjs");
+const text = "Hello, World";
+
+console.log(cowsay.say({ text }));
---
+++ Новая папка/my_package.mjs
@@ -0,0 +1,3 @@
+import { say} from "cowsay";
+const text="Hello,World";
+console.log(say({text}));
